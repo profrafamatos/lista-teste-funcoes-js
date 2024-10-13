b7ceab86f6fc076710751de07002ce554fe19efc
profrafamatos <rcmm@cesar.school>
2024-10-13 12:24:07 -0300

    Arquivos para o simulado de javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+/**** Escreva o código abaixo 👇******/
+
+
+
+
+/**** Escreva o código acima ☝️******/
+
+//Não altere as linhas abaixo
+module.exports = {
+  somar,
+  multiplicar,
+  subtrair,
+  dividir,
+  calcularMedia,
+  elevarPotencia,
+  calcularAreaRetangulo,
+  verificarParOuImpar,
+  calcularRaizQuadrada,
+  calcularHipotenusa,
+};
